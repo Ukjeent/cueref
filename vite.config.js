19a9f4d2f6cb8c43a5dc20afb162f88a1c7932d5
Ukjeent <bjorn.jakobsen91@gmail.com>
2025-06-21 12:10:07 +0200

    Update base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/cueref-react/", // Change this to just '/'
+  base: "/", // Change from '/cueref-react/' to just '/'
 });
