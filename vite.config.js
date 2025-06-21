66e06302c6286314b51618e39d130370d1d37432
Ukjeent <bjorn.jakobsen91@gmail.com>
2025-06-21 12:17:05 +0200

    Rebuild with correct base path

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Change from '/cueref-react/' to just '/'
+  base: "/cueref-react/",
 });
