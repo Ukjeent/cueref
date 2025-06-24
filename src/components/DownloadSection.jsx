60614061c9666bf71e7795de326e1cb06801243b
Ukjeent <bjorn.jakobsen91@gmail.com>
2025-06-24 11:41:36 +0200

    Minor bug fix

--- src/components/DownloadSection.jsx
+++ src/components/DownloadSection.jsx
@@ -1,25 +1,40 @@
 import "./DownloadSection.css";
 import { apiBase, endPoint } from "../config.js";
+import { useState } from "react";
 
 function DownloadSection({ uploadId, processingReady, error, setError }) {
+  const [downloadError, setDownloadError] = useState(null);
   const handleDownloadClick = async () => {
+    setDownloadError(null);
     const response = await fetch(
-      `http://localhost:8000/download-result?upload_id=${uploadId}`
+      `${apiBase}/download-result?upload_id=${uploadId}`
     );
+    if (!response.ok) {
+      setDownloadError(
+        "Something went wrong when download the result. Please try again later"
+      );
+      return;
+    } else {
+      try {
+        const blob = await response.blob();
+        // Extract filename from Content-Disposition header
+        const contentDisposition = response.headers.get("Content-Disposition");
+        const filename = contentDisposition
+          ? contentDisposition.split("filename=")[1].replace(/"/g, "")
+          : "cueref_export.xlsx"; // fallback
 
-    const blob = await response.blob();
-    // Extract filename from Content-Disposition header
-    const contentDisposition = response.headers.get("Content-Disposition");
-    const filename = contentDisposition
-      ? contentDisposition.split("filename=")[1].replace(/"/g, "")
-      : "cueref_export.xlsx"; // fallback
-
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = filename;
-    a.click();
-    window.URL.revokeObjectURL(url);
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = filename;
+        a.click();
+        window.URL.revokeObjectURL(url);
+      } catch (error) {
+        setDownloadError(
+          "Something went wrong when download the result. Please try again later"
+        );
+      }
+    }
   };
 
   return (
@@ -31,6 +46,11 @@ function DownloadSection({ uploadId, processingReady, error, setError }) {
       >
         Download Metadata
       </button>
+      {downloadError ? (
+        <p style={{ marginTop: "10px" }}>{downloadError}</p>
+      ) : (
+        ""
+      )}
     </section>
   );
 }
